2b2376ae81db7941bb79486b016245ecb7424489
zurazumba <zura.zumbadze@yahoo.com>
2023-06-21 01:43:48 +0400

    switch-1

--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,35 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year % 4 ){
+            case 0:
+                switch (year % 100){
+                    case 0:
+                        switch (year % 400){
+                            case 0:
+                                System.out.println("leap");
+                                break;
+                            default:
+                                System.out.println("not leap");
+                                break;
+                        }
+                        break;
+                    default:
+                        System.out.println("leap");
+                        break;
+                }
+            break;
+
+            default:
+                System.out.println("not leap");
+                break;
+        }
+    }
+
+    public static void main(String[] args) {
+        LeapYearPrinter leapYear = new LeapYearPrinter();
+        int year = 2000;
+        leapYear.isLeapYear(year);
     }
 }
+
